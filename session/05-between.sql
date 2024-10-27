2cee2839d1eb2a40dc86c0e8691d42f1638f5e4b
alejopua <alejopua@gmail.com>
2024-10-26 21:41:05 -0500

    Add SQL script for selecting users with a specific range of followers
    uso del parámetro between.

---
+++ session/05-between.sql
@@ -0,0 +1,22 @@
+SELECT
+	first_name,
+	last_name,
+	followers
+FROM
+	users
+WHERE
+	followers >= 4600
+	AND followers <= 4700
+ORDER BY
+	followers ASC;
+
+SELECT
+	first_name,
+	last_name,
+	followers
+FROM
+	users
+WHERE
+	followers BETWEEN 4600 AND 4700
+ORDER BY
+	followers ASC;
