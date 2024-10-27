5cb26a84f620f5bc0352a681080bbb7590bcf96b
alejopua <alejopua@gmail.com>
2024-10-26 21:35:49 -0500

    Add initial SQL script for user table and operations

---
+++ session/00-exercise.sql
@@ -0,0 +1,32 @@
+
+
+-- CREATE TABLE users (
+-- 	name VARCHAR(10) UNIQUE
+-- );
+-- INSERT INTO users (name)
+-- 		VALUES('Lleritos'),('Mariana'),('Natalia'),('Ximena'),('Paola'),('Wendy');
+-- UPDATE
+-- 	users
+-- SET
+-- 	name = 'Lauren'
+-- WHERE
+-- 	name = 'Mariana';
+-- SELECT
+-- 	*
+-- FROM
+-- 	users
+-- LIMIT 1 OFFSET 1;
+-- SELECT
+-- 	*
+-- FROM
+-- 	users
+-- WHERE
+-- 	name LIKE '%la';
+-- INSERT INTO users (name)
+-- 		VALUES('Fabio');
+-- DELETE FROM users
+-- WHERE name = 'Fabio';
+
+-- DROP TABLE userss;
+
+-- TRUNCATE TABLE userss;
