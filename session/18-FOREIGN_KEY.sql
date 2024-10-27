f1b3f161e16a30810c8cd0060c4abcaf66ff389e
alejopua <alejopua@gmail.com>
2024-10-26 21:46:29 -0500

    Add foreign key constraints to city and countrylanguage tables

---
+++ session/18-FOREIGN_KEY.sql
@@ -0,0 +1,24 @@
+ALTER TABLE city
+	ADD CONSTRAINT fk_country_code FOREIGN KEY (countrycode) REFERENCES country (code);
+
+SELECT
+	*
+FROM
+	country
+WHERE
+	code = 'AFG';
+
+SELECT
+	*
+FROM
+	city
+WHERE
+	countrycode = 'AFG';
+
+
+INSERT INTO country
+		values('AFG', 'Afghanistan', 'Asia', 'Southern Asia', 652860, 1919, 40000000, 62, 69000000, NULL, 'Afghanistan', 'Totalitarian', NULL, NULL, 'AF');
+
+
+ALTER TABLE countrylanguage
+	ADD CONSTRAINT fk_country_code FOREIGN KEY (countrycode) REFERENCES country (code);
