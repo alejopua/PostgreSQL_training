84f3667d267e427c8507d655b25b3a25cbc0909c
alejopua <alejopua@gmail.com>
2025-06-25 22:01:13 -0500

    Add SQL view to calculate the number of claps per post grouped by week, including a query to retrieve the data.

---
+++ session/Seccion_13-: Vistas_Vistas_materializadas_y_Common_Table_Expression/vistas.sql
@@ -0,0 +1,13 @@
+CREATE VIEW number_claps_per_post AS
+SELECT 
+	date_trunc('weeks', posts.created_at) as weeks, 
+	COUNT(DISTINCT posts.post_id) AS number_of_posts,
+	SUM( claps.counter) AS total_claps
+FROM posts
+INNER JOIN claps ON claps.post_id = posts.post_id
+GROUP BY weeks
+ORDER BY weeks DESC
+
+SELECT * FROM number_claps_per_post
+
+-- para borrar vista --> DROP VIEW number_claps_per_post
